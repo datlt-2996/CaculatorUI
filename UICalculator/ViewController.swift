b0556c3d83deb0fab85597be808d780ec36ddb73
Vex <luanvazz456@gmail.com>
2022-03-25 10:10:59 +0700

    first commit

---
+++ UICalculator/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  UICalculator
+//
+//  Created by Lê Tiến Đạt on 21/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var ACbutton: UIButton!
+    @IBOutlet weak var lunisolarButton: UIButton!
+    @IBOutlet weak var percentButton: UIButton!
+    @IBOutlet weak var divButton: UIButton!
+    @IBOutlet weak var multiButton: UIButton!
+    @IBOutlet weak var subButton: UIButton!
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var equalButton: UIButton!
+    @IBOutlet weak var dotButton: UIButton!
+    
+    @IBOutlet weak var zeroButton: UIButton!
+    @IBOutlet weak var oneButton: UIButton!
+    @IBOutlet weak var twoButton: UIButton!
+    @IBOutlet weak var threeButton: UIButton!
+    @IBOutlet weak var fourButton: UIButton!
+    @IBOutlet weak var fiveButton: UIButton!
+    @IBOutlet weak var sixButton: UIButton!
+    @IBOutlet weak var sevenButton: UIButton!
+    @IBOutlet weak var eightButton: UIButton!
+    @IBOutlet weak var nineButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
